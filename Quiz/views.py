5470aa8e8d69f473a088bd5674a5a39f873fed1f
sanchayjain28 <sanchayjain.it24@jecrc.ac.in>
2024-01-21 15:37:41 +0530

    neww

---
+++ Quiz/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import redirect,render
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import user_passes_test
+from .forms import *
+from .models import *
+from django.http import HttpResponse
+
+# Create your views here.
+def is_superuser(user):
+    return user.is_superuser
+
+@user_passes_test(is_superuser)
+def superuser_home(request):
+    questions = QuesModel.objects.all()
+    context = {
+        'questions': questions
+    }
+    return render(request, 'Quiz/superuser_home.html', context)
+def home(request):
+    if request.user.is_superuser:
+            # If the user is a superuser, render a different template
+            return superuser_home(request)
+    if request.user.is_anonymous:
+        # If the user is anonymous, render another home template
+        return render(request, 'Quiz/login.html')
+    if request.method == 'POST':
+        print(request.POST)
+        questions=QuesModel.objects.all()
+        score=0
+        wrong=0
+        correct=0
+        total=0
+        for q in questions:
+            total+=1
+            print(request.POST.get(q.question))
+            print(q.ans)
+            print()
+            if q.ans ==  request.POST.get(q.question):
+                score+=10
+                correct+=1
+            else:
+                wrong+=1
+        percent = score/(total*10) *100
+        context = {
+            'score':score,
+            'time': request.POST.get('timer'),
+            'correct':correct,
+            'wrong':wrong,
+            'percent':percent,
+            'total':total
+        }
+        return render(request,'Quiz/result.html',context)
+    else:
+        questions=QuesModel.objects.all()
+        context = {
+            'questions':questions
+        }
+        return render(request,'Quiz/home.html',context)
+
+def addQuestion(request):    
+    if request.user.is_staff:
+        form=addQuestionform()
+        if(request.method=='POST'):
+            form=addQuestionform(request.POST)
+            if(form.is_valid()):
+                form.save()
+                return redirect('/')
+        context={'form':form}
+        return render(request,'Quiz/addQuestion.html',context)
+    else: 
+        return redirect('home') 
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home') 
+    else: 
+        form=createuserform()
+        if request.method=='POST':
+            form=createuserform(request.POST)
+            if form.is_valid() :
+                user=form.save()
+                return redirect('login')
+        context={
+            'form':form,
+        }
+        return render(request,'Quiz/register.html',context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+       if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+       context={}
+       return render(request,'Quiz/login.html',context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('/')
+
